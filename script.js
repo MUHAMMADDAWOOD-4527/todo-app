b5ea6b407faa76ba792246017048b1df49c7e1e7
MUHAMMADDAWOOD-4527 <muhammaddawoodqureshi33@gmail.com>
2024-12-22 12:07:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('task-input')
+const addTaskBtn = document.getElementById('add-task-btn')
+const taskList = document.getElementById('task-list')
+const clearCompleted = document.getElementById('clear-completed')
+
